a40957ca9be8841fc568f2a10a5d3642aa9914b2
Your Name <you@example.com>
2024-01-02 14:20:25 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 void stat(const double[],int,double[]);
 
+
+
 int main(){
     double A[] = {1.2,3.5,6.9,7.8,12.5,0.5};
     int N = sizeof(A)/sizeof(A[0]);
@@ -16,4 +18,57 @@ int main(){
     cout << "\nMax = " << B[4];
     cout << "\nMin = " << B[5];
     return 0;
+}
+
+void stat(const double A[], int size, double B[]){
+   
+    //Arithmetic Mean
+    
+    double sum = 0;
+    for (int i = 0; i < size; ++i) {
+        sum += A[i];
+    }
+    B[0] = sum / size;
+   
+    //Standard Deviation
+   
+    double sumSquaredDiff = 0;
+    for (int i = 0; i < size; ++i) {
+        sumSquaredDiff += pow(A[i] - B[0], 2);
+    }
+    B[1] = sqrt(sumSquaredDiff / size);
+    
+    //Geometric Mean
+   
+    double product = 1.0;
+    for (int i = 0; i < size; ++i) {
+        product *= A[i];
+    }
+    B[2] = pow(product, 1.0 / size);
+   
+    //Harmonic Mean
+    
+    double sumReciprocal = 0;
+    for (int i = 0; i < size; ++i) {
+        sumReciprocal += 1.0 / A[i];
+    }
+    B[3] = size / sumReciprocal;
+
+    //Max and Min
+
+    double maxVal = A[0];
+    double minVal = A[0];
+
+    for (int i = 1; i < size; ++i) {
+        if (A[i] > maxVal) {
+            maxVal = A[i];
+        }
+        if (A[i] < minVal) {
+            minVal = A[i];
+        }
+    }
+    B[4] = maxVal;
+    B[5] = minVal;
+
+
 }
