1cf17bd61f205bbe840e653bb10dfe0a5553d3dc
Your Name <you@example.com>
2024-01-02 15:21:29 +0700

    13_2.cpp completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,40 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool image[][M])
+{
+    // Display top border
+    for (int i = 0; i < M + 2; ++i) {
+        cout << '-';
+    }
+    cout << endl;
+
+    // Display image with left and right borders
+    for (int i = 0; i < N; ++i) {
+        cout << '|';  // Left border
+        for (int j = 0; j < M; ++j) {
+            cout << (image[i][j] ? '*' : ' ');
+        }
+        cout << '|' << endl;  // Right border
+    }
+
+    // Display bottom border
+    for (int i = 0; i < M + 2; ++i) {
+        cout << '-';
+    }
+    cout << endl;
+}
+
+void updateImage(bool image[][M], int brushSize, int x, int y)
+{
+    // Update the image based on the brush size and location
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            double distance = sqrt(pow(i - x, 2) + pow(j - y, 2));
+            if (distance <= brushSize - 1) {
+                image[i][j] = true;
+            }
+        }
+    }
+}
